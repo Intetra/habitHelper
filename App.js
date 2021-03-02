0ee84821976b275f4443c9938c6c4675c6946389
aubrey <intetra@gmail.com>
2021-03-02 11:25:34 -0700

    firebase api key added and hidden

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as firebase from 'firebase'
+import firebaseConfig from './firebaseConfig.js'
+
 
 export default function App() {
+  const fbConfig = firebaseConfig()
+
+  // Initialize Firebase
+  !firebase.apps.length ? firebase.initializeApp(fbConfig) : firebase.app();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Habit Helper</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
