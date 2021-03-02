929750e50acf54ab26a3bc59c94873433c966890
aubrey <intetra@gmail.com>
2021-03-02 14:10:55 -0700

    got firebase authentication and firestore working.

--- App.js
+++ App.js
@@ -1,29 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import * as firebase from 'firebase'
-import firebaseConfig from './firebaseConfig.js'
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import * as firebase from 'firebase';
+import firebaseConfig from './src/api/firebaseConfig';
+import WelcomeScreen from './src/screens/WelcomeScreen';
+import SignUp from './src/screens/SignUp';
+import SignIn from './src/screens/SignIn';
+import LoadingScreen from './src/screens/LoadingScreen';
+import Dashboard from './src/screens/Dashboard';
 
+const Stack = createStackNavigator();
 
 export default function App() {
-  const fbConfig = firebaseConfig()
-
-  // Initialize Firebase
-  !firebase.apps.length ? firebase.initializeApp(fbConfig) : firebase.app();
+  if (!firebase.apps.length) {
+    console.log('Connected with Firebase')
+    firebase.initializeApp(firebaseConfig());
+  }
 
   return (
-    <View style={styles.container}>
-      <Text>Habit Helper</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={'Loading'} component={LoadingScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name='Home' component={WelcomeScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name='Sign Up' component={SignUp} options={{ headerShown: true }}/>
+        <Stack.Screen name='Sign In' component={SignIn} options={{ headerShown: true }}/>
+        <Stack.Screen name={'Dashboard'} component={Dashboard} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
